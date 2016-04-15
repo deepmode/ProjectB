992eb5771789ae991a685b9f4d1dacda20e7924a
Eric Ho <ericho@101MediaLabs-iMac-Ho-Ho.local>
2016-04-15 17:06:45 +0800

    no message

---
+++ ProjectB/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ProjectB
+//
+//  Created by EricHo on 14/3/2016.
+//  Copyright © 2016 E H. All rights reserved.
+//
+
+import UIKit
+import EHSharecode
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayView1: UIView!
+    @IBOutlet weak var displayView2: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func changeColorButtonPress(sender: AnyObject) {
+        self.displayView1.backgroundColor = UIColor.randomColor
+        self.displayView2.backgroundColor = UIColor.randomColor
+    
+        
+    }
+
+}
+
